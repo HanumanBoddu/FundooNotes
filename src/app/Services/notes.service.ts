1667f532983fde3bdda292a824da851086df4a7b
Hanuman Boddu <Hanuman.Boddu@godigit.com>
2024-08-20 10:48:01 +0530

    Added Methods to call API

---
+++ src/app/Services/notes.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpService } from './http.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NotesService {
+  constructor(private httpService:HttpService) { }
+  baseurl:string="notes";
+  access_token=localStorage.getItem("access_token")
+  getAllNotes(url:any){
+    return this.httpService.GetService(`${this.baseurl}/${url}?access_token=${this.access_token}`);
+  }
+  addNotes(url:any,data:any){
+    return this.httpService.pApiCall(`${this.baseurl}/${url}?access_token=${this.access_token}`,data);
+  }
+}
